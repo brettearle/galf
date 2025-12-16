685625dd38aaace80b59df517d029e0db616e497
brett <brettearle@gmail.com>
2025-12-17 05:40:34 +1030

    added get_all endpoint

---
+++ cmd/api/internal/handlers/get_all.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	fl "github.com/brettearle/galf/internal/flag"
+)
+
+func GetAll(f *fl.Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		flags, err := f.GetAll(r.Context())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		jsonFlag, err := json.Marshal(flags)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to get flag, please contact admin"))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonFlag)
+	}
+}
